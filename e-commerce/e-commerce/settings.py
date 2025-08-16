4d8aab2a11badb0becf8e9674a12b19775fca8a0
pawan kumar gond <98225403+kpawangond@users.noreply.github.com>
2025-08-16 10:51:06 +0530

    Update settings.py

--- e-commerce/e-commerce/settings.py
+++ e-commerce/e-commerce/settings.py
@@ -104,13 +104,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static",   # this line gives the warning
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
